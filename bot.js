2f171bb46c3622e388398b31734b07b7ca72d1b3
pedroestrada.86@gmail.com <pedroestrada.86@gmail.com>
2017-05-20 22:46:45 -0400

    no negative look behinds in javascript

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/,
-      wooRegex = /(?<![\w\d])wo+(?![\w\d])/;
+      wooRegex = /\bwoo+\b/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
