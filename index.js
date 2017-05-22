d65a18bfb1cfa4ebaed10cf9324aaa3b70a3c269
pedroestrada.86@gmail.com <pedroestrada.86@gmail.com>
2017-05-21 23:03:31 -0400

    adding discord test 1

--- index.js
+++ index.js
@@ -31,3 +31,18 @@ function ping() {
   this.res.writeHead(200);
   this.res.end("Hey, I'm Cool Guy.");
 }
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login('316039085993230337');
